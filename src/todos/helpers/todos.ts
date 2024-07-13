bf037438ef1bf275fb78f22dc31d3523eb053581
leandroquiroga <leandroquiroga9514@gmail.com>
2024-07-13 20:12:44 -0300

    Se agrega nuevo endpoint - DELETE

--- src/todos/helpers/todos.ts
+++ src/todos/helpers/todos.ts
@@ -1,5 +1,15 @@
 import { Todo } from "@prisma/client";
 
+// TODO: Implementar para una mejor experiencia de usuario
+const sleep = (second = 0): Promise<boolean> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, second * 1000);
+  });
+
+}
+
 export const updateTodo = async (id: string, done: boolean): Promise<Todo> => {
 
   const body = { done };
@@ -12,6 +22,35 @@ export const updateTodo = async (id: string, done: boolean): Promise<Todo> => {
     },
   }).then((res) => res.json());
 
-  console.log(todo)
+
   return todo;
 };
+
+export const createTodo = async (description: string): Promise<Todo> => {
+
+  const body = { description };
+
+  const todo = await fetch(`/api/todo`, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+
+  return todo;
+};
+
+
+export const deleteTodoCompleted = async (): Promise<Todo[]> => {
+
+  const todos = await fetch(`/api/todo`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  return todos;
+}
