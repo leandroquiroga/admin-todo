fba4ff706ab084083cf4726bd3075df77af65d7f
leandroquiroga <leandroquiroga9514@gmail.com>
2024-08-14 22:36:24 -0300

    Se agrega logica que controla el carrito de compras desde el componente <Product />

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -2,7 +2,7 @@ import { ProductCard } from "@/components";
 
 export default function ProductPage() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap2">
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap2">
       <ProductCard />
     </div>
   );
--- src/products/components/Products.tsx
+++ src/products/components/Products.tsx
@@ -1,15 +1,66 @@
 "use client";
-import React, { useState } from "react";
+
+import React, { MouseEvent, useEffect, useState } from "react";
 import Image from "next/image";
-import { Product } from "../interfaces";
+import { AviableSizes, Product } from "../interfaces";
+import { addProductToCart } from "@/shopping-cart/actions/actions";
+import { useRouter } from "next/navigation";
 
+let countClick = 0;
 export const Products = ({ product }: Product) => {
-  const { image, name, price, description, avaibleSizes } = product;
-  const [sizeSelected, setSizeSelected] = useState<string>("");
+  const router = useRouter();
 
-  const handleSize = (size: string) => {
+  const { image, name, price, description, avaibleSizes, id } = product;
+  const [sizeSelected, setSizeSelected] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [disableButton, setDisableButton] = useState<boolean>(false);
+  const [sizeAvailable, setSizeAvailable] = useState<string>("");
+
+  const handleSize = (
+    e: MouseEvent<HTMLInputElement>,
+    value: AviableSizes,
+    index: number
+  ) => {
+    countClick += 1;
+    const { count, size } = value;
     setSizeSelected(size);
+
+    // Averiguamos si el contador es mayor a 0
+    if (countClick > 1 && count === 0) {
+      setSizeAvailable("No disponible");
+      setDisableButton(true);
+      return;
+    }
+    if (count === 0) {
+      const element = e.target as HTMLInputElement;
+      const textContentDispo =
+        element.parentElement?.parentElement?.parentElement?.parentElement
+          ?.childNodes[3];
+      setSizeAvailable(
+        textContentDispo?.childNodes[index].textContent as string
+      );
+      setDisableButton(true);
+      return;
+    }
+
+    setDisableButton(false);
+    return;
   };
+
+  const addToCart = () => {
+    if (!sizeSelected) {
+      setErrorMessage("Seleccione un talle por favor!");
+      return;
+    }
+    // setDisableButton(false);
+    setErrorMessage("");
+    addProductToCart(id);
+    router.refresh();
+  };
+
+  const isSizeAvailable = (): boolean =>
+    sizeAvailable === "No disponible" ? true : false;
+
   return (
     <>
       <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md my-2">
@@ -35,18 +86,25 @@ export const Products = ({ product }: Product) => {
             {description}
           </p>
         </div>
-        <div className="flex items-center justify-center mt-4 mb-6 pb-6 border-b border-slate-200">
-          {avaibleSizes.map((value) => (
-            <div className="space-x-2 flex flex-row justify-center items-center text-sm">
+        <div className="flex items-center justify-center my-2 mx-4 p-2 border-slate-200">
+          {avaibleSizes.map((value, index) => (
+            <div
+              className="space-x-4 flex flex-row justify-center items-center text-sm"
+              key={index}>
               <label>
                 <input
                   className="sr-only peer"
                   name="size"
                   type="radio"
                   value={sizeSelected}
-                  onClick={() => handleSize(value.size)}
+                  onClick={(e) => handleSize(e, value, index)}
                 />
-                <div className="w-9 h-9 cursor-pointer rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+                <div
+                  className={`w-9 h-9 cursor-pointer rounded-lg flex items-center justify-center text-slate-700 peer-checked:text-white ${
+                    sizeSelected
+                      ? "peer-checked:font-semibold peer-checked:bg-slate-900"
+                      : ""
+                  } `}>
                   {value.size}
                 </div>
               </label>
@@ -54,24 +112,35 @@ export const Products = ({ product }: Product) => {
           ))}
         </div>
         {sizeSelected && (
-          <div className="flex items-center justify-center mt-4 mb-6 pb-6 border-b border-slate-200">
-            {avaibleSizes.map((value) => (
-              <div className="space-x-2 text-sm">
-                {sizeSelected === value.size && (
-                  <span className="font-semibold text-xs text-center">
-                    Disponibilidad: {value.count}
-                  </span>
-                )}
-              </div>
+          <div className="text-center p-2 border-b border-slate-200">
+            {avaibleSizes.map((value, index) => (
+              <span
+                key={index}
+                className={`font-semibold text-xs text-center ${
+                  value.count === 0 ? "text-red-400" : ""
+                }`}>
+                {sizeSelected === value.size &&
+                  (value.count === 0
+                    ? "No disponible"
+                    : `Disponibilidad: ${value.count} `)}
+              </span>
             ))}
           </div>
         )}
-        <div className="p-6 pt-0">
+        <div className="p-4 flex flex-col justify-center items-center">
           <button
+            disabled={disableButton}
+            onClick={addToCart}
             className="block border w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button">
             Add to Cart
           </button>
+          {!sizeSelected ||
+            (sizeAvailable === "No disponible" && (
+              <span className="text-sm text-red-400 text-center my-2">
+                {errorMessage}
+              </span>
+            ))}
         </div>
       </div>
     </>
