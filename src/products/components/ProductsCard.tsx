09baa045a537f58c0c1ed36c68115991433a8d59
leandroquiroga <leandroquiroga9514@gmail.com>
2024-08-16 23:25:18 -0300

    Se agragan nuevos estilos y se modifican otros

--- src/products/components/ProductsCard.tsx
+++ src/products/components/ProductsCard.tsx
@@ -1,14 +1,34 @@
+"use client";
+import { useContext } from "react";
 import { products } from "../data";
 import { Products } from "./Products";
+import { UIContext } from "@/context";
+import { CartProduct } from "@/shopping-cart/components/CartProduct";
 
-export const ProductCard = () => {
+import styles from "./shoppingCart.module.css";
+import { ProductInCartProps } from "../interfaces";
+
+interface ProductCardProps {
+  productInCartList: ProductInCartProps[];
+}
+export const ProductCard = ({ productInCartList }: ProductCardProps) => {
+  const { showCart } = useContext(UIContext);
+
   return (
     <>
-      {
-        products.map((product) => (
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[3rem] ">
+        {products.map((product) => (
           <Products key={product.id} product={product} />
-        ))
-      }
+        ))}
+      </div>
+      <div
+        className={`${
+          showCart ? styles.cartOpen : styles.cartClose
+        } relative mt-10 w-full md:w-1/3 `}>
+        {productInCartList.map(({ product, quantity }) => (
+          <CartProduct key={product.id} product={product} quantity={quantity} />
+        ))}
+      </div>
     </>
-  )
+  );
 };
