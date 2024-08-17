1730db4b96d93daa5cc59cc58c9f0ad64051f98e
leandroquiroga <leandroquiroga9514@gmail.com>
2024-08-16 23:24:23 -0300

    Se agrega funcionalidad para leer los productos que tenemos en nuestras cookies.

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,9 +1,37 @@
 import { ProductCard } from "@/components";
+import { products } from "@/products/data";
+import { ProductInCartProps } from "@/products/interfaces";
+import { CookiesCart } from "@/shopping-cart/actions/actions";
+import { cookies } from "next/headers";
+
+export const metadata = {
+  title: "Product Page",
+  description: "Product page",
+};
+
+const productInCart = (cart: CookiesCart): ProductInCartProps[] => {
+  const productInCart: ProductInCartProps[] = [];
+
+  // Recorremos el carrito y buscamos los productos correspondiente al id del producto
+  for (const id of Object.keys(cart)) {
+    const product = products.find((product) => product.id === id);
+
+    //si el producto existe lo agregamos al arreglo del carrito
+    if (product) {
+      productInCart.push({ product, quantity: cart[id] });
+    }
+  }
+
+  return productInCart;
+};
 
 export default function ProductPage() {
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[3rem] ">
-      <ProductCard />
-    </div>
-  );
+  const cookiesStore = cookies();
+  const cart = JSON.parse(
+    cookiesStore.get("cart")?.value ?? "{}"
+  ) as CookiesCart;
+
+  const productInCartList = productInCart(cart);
+
+  return <ProductCard productInCartList={productInCartList} />;
 }
